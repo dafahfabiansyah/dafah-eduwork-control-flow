508fc20307378818e84c5eb5386de8fb88c468a5
Dafah fabian syah <121594681+dafahfabiansyah@users.noreply.github.com>
2023-05-31 12:11:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ while (printGrade) {
   printGrade = prompt('Input Nilai:');
   if (printGrade < 50) {
     alert('Nilai Anda C');
-    printGrade = confirm('Lanjutkan?');
   }
-  if ((printGrade = 50)) {
+  if (printGrade == 50) {
     alert('Nilai Anda B');
-    printGrade = confirm('Lanjutkan?');
   }
-  if (printGrade > 50) {
+  if (printGrade > 60) {
     alert('Nilai Anda A');
+  } else {
     printGrade = confirm('Lanjutkan?');
   }
 }
